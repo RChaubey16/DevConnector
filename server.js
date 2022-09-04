e2d4e9471d1f64283a91b3ed8e6596dd6c3b7eb5
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2022-09-04 18:33:48 +0530

    Added the necessary routes for the application.

--- server.js
+++ server.js
@@ -8,5 +8,11 @@ connectDB();
 // A simple get request whick takes a callback function with the request and response as arguments.
 app.get("/", (req, res) => res.send(`API up and running...`));
 
+// Define routes.
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT : ${PORT}`));
