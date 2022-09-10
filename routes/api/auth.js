19125667b04c1906c572e26014fbdfaa6ed86d3b
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2022-09-10 19:30:59 +0530

    Created a custom auth middleware.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../../middleware/auth");
+const User = require("../../models/User");
+
+// @route     GET api/auth/sample
+// @desc      Sample route
+// @access    Public
+router.get("/sample", (req, res) => res.send(`sample Route...`));
 
 // @route     GET api/auth
 // @desc      Test route
 // @access    Public
-router.get("/", (req, res) => res.send(`Auth Route...`));
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error.");
+  }
+});
 
 module.exports = router;
