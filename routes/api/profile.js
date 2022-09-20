d4f4df1c8fb8fa1120b1dc5ed83dc686da9b3e9d
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2022-09-20 19:34:15 +0530

    Integrated the GitHub API and fetched the repos.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const authMiddleware = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
@@ -302,6 +304,38 @@ router.delete("/education/:edu_id", authMiddleware, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error.");
+  }
+});
+
+// @route     GET api/profile/github/:username
+// @desc      GET user repos from github
+// @access    Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: {
+        "user-agent": "node.js",
+      },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error.message);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No GitHub Profile found." });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server error.");
