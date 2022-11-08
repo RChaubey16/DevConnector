70463f4bfed76fde599085997dd6078de3270211
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2022-11-08 12:43:57 +0530

    React router setup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/register" exact element={<Register />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
