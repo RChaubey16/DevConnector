3107b517f2bc5da4ece010a176de9ff3cc32480e
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2023-01-02 19:48:49 +0530

    Redux setup - boiler plate.

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,28 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import "./App.css";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Landing />} />
-        </Routes>
-        <section className="container">
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Routes>
-            <Route path="/login" exact element={<Login />} />
-            <Route path="/register" exact element={<Register />} />
+            <Route path="/" exact element={<Landing />} />
           </Routes>
-        </section>
-      </Fragment>
-    </Router>
+          <section className="container">
+            <Routes>
+              <Route path="/login" exact element={<Login />} />
+              <Route path="/register" exact element={<Register />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
