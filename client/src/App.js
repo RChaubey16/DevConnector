ff7ca8b6e1660b1bfd4fda87bb6c07af22b63b1e
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2023-01-06 21:27:47 +0530

    Created the dashboard component and also the PrivateComponent component.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -34,6 +36,15 @@ const App = () => {
             <Routes>
               <Route path="/login" exact element={<Login />} />
               <Route path="/register" exact element={<Register />} />
+              <Route
+                path="/dashboard"
+                exact
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
             </Routes>
           </section>
         </Fragment>
