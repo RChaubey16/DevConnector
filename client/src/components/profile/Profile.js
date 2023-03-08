0f7054495e52cc729b9b62903744ac18c817d1e7
ruturaj.chaubey <ruturaj.chaubey@qed42.com>
2023-03-08 19:48:54 +0530

    Added the profile-top and profile-about components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
@@ -13,17 +15,27 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 
   return (
     <Fragment>
-      {profile == null || loading ? <Spinner /> : <Fragment></Fragment>}
-      <Link to="/profiles" className="btn btn-light">
-        Back to Profiles
-      </Link>
-      {auth?.isAuthenticated &&
-        auth?.loading === false &&
-        auth?.user?._id === profile?.user?._id && (
-          <Link to="/edit-profile" className="btn btn-dark">
-            Edit Profile
+      {profile == null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          {" "}
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
           </Link>
-        )}
+          {auth?.isAuthenticated &&
+            auth?.loading === false &&
+            auth?.user?._id === profile?.user?._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+          <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
